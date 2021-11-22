2df11fd7c98f2cf8d4d24878c3f53055b1f2d806
Rassokhin Anton <antonrassohin@gmail.com>
2021-11-22 23:37:20 +0300

    Добавьте файлы проекта.

---
+++ Lab 6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи строку:");
+            string aString = Console.ReadLine();
+            string[] sArray = aString.Split();
+            int max = sArray[0].Length;
+            
+            foreach (string s in sArray)
+            {
+                int sC = s.Length;
+                if (sC > max)
+                    max = sC;
+            }
+            Console.WriteLine("Самое длинное слово -{0} букв",max);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задание 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи строку:");
+            string aString = Console.ReadLine();
+            string cString = aString.ToLower();
+            string[] splArray = cString.Split();
+            string bString = String.Join("", splArray);
+
+            char[] symArray = bString.ToCharArray();
+            Array.Reverse(symArray);
+            string revString = String.Join("", symArray);
+
+            if (bString == revString)
+            {
+                Console.WriteLine("Ура! Палиндром!");
+            }
+            else
+            {
+                Console.WriteLine("Не палиндром(");
+            }                    
+            Console.ReadKey();
+        }
+    }
+}
